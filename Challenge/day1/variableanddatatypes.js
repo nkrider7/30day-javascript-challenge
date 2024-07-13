d95f25dab6abd3a758a4b7353cc15a04f2deaab1
narendra <narendranishad59@gmail.com>
2024-07-13 10:00:50 +0530

    Day1 completed

---
+++ Challenge/day1/variableanddatatypes.js
@@ -0,0 +1,68 @@
+// Activity 1: Variable Declaration 
+
+var Age = 19;
+console.log(Age)
+console.log( typeof Age)
+
+let Name = "Peter";
+console.log(Name)
+console.log(typeof Name)
+
+// Activity 2: constant declaration
+
+const isLove = true 
+console.log(isLove)
+console.log(typeof isLove)
+
+// Activity 3: Data_Types
+
+// TASK 4
+// Create variable of different dataTypes, and log variable's type using the 'typeof' operator.
+
+//primitive and non-primitive dataTypes
+// primitive - Number, String, Boolean
+
+// Number
+console.log('check typeof for number')
+const id = 1234
+let weight = 32.5
+console.log(typeof id);
+console.log(typeof weight);
+
+// String
+console.log('check typeof for string')
+const username = 'Peter Parkar'
+let role = "contributor"
+console.log(typeof username);
+console.log(typeof role);
+
+// Boolean
+
+console.log('check typeof for boolean')
+const userLoggedin = true
+const userLoggedout = false
+let isLoggedIn = true
+
+console.log(typeof userLoggedin)
+console.log(typeof userLoggedout)
+console.log(typeof isLoggedIn)
+
+// Non-Primitive - Array, Object
+
+// Array
+console.log('check typeof for Array')
+let colourSet = ['red', 'green', 'blue', 'white', 'black']
+console.log(colourSet)
+console.log(typeof colourSet);
+
+
+// Object
+console.log('check typeof for Object')
+
+let obj = {
+    FullName: "Petter Parker",
+    Age: 16
+}
+
+console.log(obj)
+console.log(typeof obj)
